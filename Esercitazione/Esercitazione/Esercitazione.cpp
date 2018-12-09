da3171d94f159722c686ba0fca9e47034a25ade3
Michele <michi01xy@gmail.com>
2018-12-09 13:24:13 +0100

    Basta

--- Esercitazione/Esercitazione/Esercitazione.cpp
+++ Esercitazione/Esercitazione/Esercitazione.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() {
 	for (int i = 0; i < 5; i++) {
-		cout <<"Variabile contatore: ";
+		cout <<"Variabile contatore 12: ";
 		cout << i << endl;
 	}
 	system("pause");
